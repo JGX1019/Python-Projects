5b84d58205ace43b5cc72607b1e65ea1ae933e77
JGX1019 <guptajai.5335@gmail.com>
2024-10-14 21:30:43 +0530

    Program #7

---
+++ #07 Madlibs/main.py
@@ -0,0 +1,13 @@
+adjective = input("Adjective: ")
+name = input("A Name: ")
+noun = input("Noun: ")
+object = input("object: ")
+plural_noun = input("Plural noun: ")
+adjective2 = input("Adjective 2: ")
+animal = input("animal: ")
+verb = input("verb ending in -ing: ")
+verb2 = input("verb: ")
+adjective3 = input("Adjective 3: ")
+noun2 = input("Noun 2: ")
+
+print(f"\nOnce upon a time, a(n) {adjective} explorer named {name} set off on a quest to find the legendary {noun}. Armed with a(n) {object} and a bag of {plural_noun}, they ventured into the {adjective2} forest. Along the way, they encountered a(n) {animal} doing a(n) {verb} dance. After hours of {verb2}, the explorer finally found the treasure, which turned out to be a(n) {adjective3} {noun2}")
