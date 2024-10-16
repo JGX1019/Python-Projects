2087d9ac34260faf5d79099ff481c106ce7c562c
JGX1019 <guptajai.5335@gmail.com>
2024-10-16 21:11:21 +0530

    Program #9

---
+++ #09 Photo Editor/main.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = "Python-Projects\\#09 Photo Editor\\Images"
+pathout = "Python-Projects\\#09 Photo Editor\\Edited_Images"
+
+if not os.path.exists(path):
+    os.makedirs(path)
+if not os.path.exists(pathout):
+    os.makedirs(pathout)
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    for filename in os.listdir(path):
+    
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+            img = Image.open(os.path.join(path, filename))
+
+            sharpness_enhancer = ImageEnhance.Sharpness(img)
+            img = sharpness_enhancer.enhance(1.5)  
+            
+            contrast_enhancer = ImageEnhance.Contrast(img)
+            img = contrast_enhancer.enhance(1.2)  
+            
+            brightness_enhancer = ImageEnhance.Brightness(img)
+            img = brightness_enhancer.enhance(1.1)  
+            
+            color_enhancer = ImageEnhance.Color(img)
+            img = color_enhancer.enhance(1.3)  
+
+            img = img.filter(ImageFilter.GaussianBlur(radius=0.5)) 
+
+            cleanname = os.path.splitext(filename)[0]
+            extension = os.path.splitext(filename)[1]
+
+            img.save(os.path.join(pathout, f"{cleanname}_edited{extension}"))
+
+print("Processing completed.")
